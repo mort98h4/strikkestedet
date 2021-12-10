53a5a0a6d3df3203a67a8cd893a9f47d8ef934a6
Morten Gross <mg@vuffeli.dk>
2021-12-10 23:24:55 +0100

    Added Head tag from next

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,15 @@
 import '../styles/globals.css';
 import '../styles/app.css';
+import Head from 'next/head'
 import Header from '../globals/Header';
 import Footer from '../globals/Footer';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Strikkestedet</title>
+      </Head>
       <Header></Header>
       <main className="2xl:container mx-auto bg-background">
         <Component {...pageProps} />
