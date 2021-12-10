47c89e5ec75f5feaf9b9049580d9b78ee2d31c30
Morten Gross <mg@vuffeli.dk>
2021-12-10 23:17:51 +0100

    Added the old header

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,15 @@
+// import "tailwindcss/tailwind.css";
 import '../styles/globals.css';
 import '../styles/app.css';
+import Header from '../globals/Header';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header></Header>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
