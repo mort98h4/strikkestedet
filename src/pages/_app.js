e36d990a82382c998fded5e521c9cb10dd7e4e97
Morten Gross <mg@vuffeli.dk>
2021-12-10 23:20:22 +0100

    Added old footer

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,14 @@
-// import "tailwindcss/tailwind.css";
 import '../styles/globals.css';
 import '../styles/app.css';
 import Header from '../globals/Header';
+import Footer from '../globals/Footer';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Header></Header>
       <Component {...pageProps} />
+      <Footer></Footer>
     </>
   )
 }
