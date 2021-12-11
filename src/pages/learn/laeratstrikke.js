9149d46af9574085fa59d348fa88eb394a996442
Morten Gross <mg@vuffeli.dk>
2021-12-11 14:13:37 +0100

    initial page commit

---
+++ src/pages/learn/laeratstrikke.js
@@ -0,0 +1,12 @@
+export default function LearnToKnit() {
+    return (
+        <>
+            <section className="grid grid-cols-6 gap-4">
+                <article className="col-span-6 lg:col-span-3 lg:col-start-2 p-16">
+                    <h1 className="font-serif text-3xl lg:text-5xl mb-4">test</h1>
+                    <p>Dette er en test</p>
+                </article>
+            </section>
+        </>
+    )
+}
