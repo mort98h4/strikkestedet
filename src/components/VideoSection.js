be6110021b3941d8c6cba76f5dbf918dba6a3163
Morten Gross <mg@vuffeli.dk>
2021-12-11 15:51:07 +0100

    Fixed some attributes

--- src/components/VideoSection.js
+++ src/components/VideoSection.js
@@ -4,7 +4,7 @@ export default function VideoSection(props) {
         <section className="w-full mb-4">
             <article className="grid grid-cols-6 gap-4 flex flex-wrap">
                 <div className={"col-span-6 lg:col-span-3 embed-responsive embed-responsive-16by9 " + ((props.index % 2 > 0) ? "lg:order-2" : "lg:order-1")}>
-                    <iframe className="embed-responsive-item" src={props.url} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe className="embed-responsive-item" src={props.url} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                 </div>
                 <div className={"col-span-6 lg:col-span-3 p-16 self-center " + ((props.index % 2 > 0) ? "lg:order-1" : "lg:order-2")}>
                     <span className="font-sans text-sm text-black-60">{props.videoNumber}.</span>
