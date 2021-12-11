6079f048e727f8b11751ecfa4759ba4f7e27f2f6
Morten Gross <mg@vuffeli.dk>
2021-12-11 14:47:23 +0100

    Video section component is rendered with mock content

---
+++ src/components/VideoSection.js
@@ -0,0 +1,17 @@
+export default function VideoSection(props) {
+    console.log(props);
+    return (
+        <section className="w-full mb-4">
+            <article className="grid grid-cols-6 gap-4 flex flex-wrap">
+                <div className={"col-span-6 lg:col-span-3 embed-responsive embed-responsive-16by9 " + ((props.index % 2 > 0) ? "lg:order-2" : "lg:order-1")}>
+                    <iframe className="embed-responsive-item" src={props.url} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                <div className={"col-span-6 lg:col-span-3 p-16 self-center " + ((props.index % 2 > 0) ? "lg:order-1" : "lg:order-2")}>
+                    <span className="font-sans text-sm text-black-60">{props.videoNumber}.</span>
+                    <h2 className="font-serif text-xl lg:text-3xl mb-4">{props.header}</h2>
+                    <p className="text-black-60">{props.body}</p>
+                </div>
+            </article>
+        </section>
+    )
+}
--- src/pages/learn/laeratstrikke.js
+++ src/pages/learn/laeratstrikke.js
@@ -1,8 +1,20 @@
-import { getLearnToKnitPage } from "../../../lib/api" 
+import { getLearnToKnitPage } from "../../../lib/api" ;
+import VideoSection from "../../components/VideoSection";
 
 export default function LearnToKnit(props) {
     console.log(props);
     const section1 = props.content.page.ltksection1;
+
+    const array = [
+        {
+            id: "432ds2",
+            url: "https://www.youtube.com/embed/uE-1RPDqJAY",
+            header: "Overskrift",
+            body: "Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+            videoNumber: 1
+        }
+    ]
+
     return (
         <>
             <section className="grid grid-cols-6 gap-4">
@@ -11,6 +23,10 @@ export default function LearnToKnit(props) {
                     <p className="text-black-60">{section1.body1}</p>
                 </article>
             </section>
+
+            {array.map((obj) => {
+                return <VideoSection key={obj.id} index={array.findIndex(item => item.id === obj.id)} {...obj}></VideoSection>
+            })}
         </>
     )
 }
