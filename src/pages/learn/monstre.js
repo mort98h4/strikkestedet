5a090b6a268fa5553a093ea7389a9c1c8a1cd3dc
Morten Gross <mg@vuffeli.dk>
2021-12-11 16:55:25 +0100

    Implemented basic content

---
+++ src/pages/learn/monstre.js
@@ -0,0 +1,32 @@
+import { getPatternsPage } from "../../../lib/api" ;
+import VideoSection from "../../components/VideoSection";
+
+export default function Patterns(props) {
+    console.log(props);
+    const section1 = props.content.page.learnsection1;
+    // const learningVideos = props.content.posts.edges;
+
+    return (
+        <>
+            <section className="grid grid-cols-6 gap-4">
+                <article className="col-span-6 lg:col-span-3 lg:col-start-2 p-16">
+                    <h1 className="font-serif text-3xl lg:text-5xl mb-4">{props.content.page.title}</h1>
+                    <p className="text-black-60">{section1.body1}</p>
+                </article>
+            </section>
+
+            {/* {learningVideos.map((video) => {
+                return <VideoSection key={video.node.id} index={learningVideos.findIndex(item => item.node.id === video.node.id)} {...video}></VideoSection>
+            })} */}
+        </>
+    )
+}
+
+export async function getStaticProps() {
+    const content = await getPatternsPage();
+    return {
+        props: {
+            content,
+        }
+    }
+}
