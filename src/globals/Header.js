dcac19204b878a867720ff7eb03f4743f67cb061
Morten Gross <mg@vuffeli.dk>
2021-12-12 23:23:37 +0100

    Navigation now works on mobile

--- src/globals/Header.js
+++ src/globals/Header.js
@@ -18,8 +18,18 @@ export default function Header(props) {
     {
       id: 3,
       visible: false,
+    },
+    {
+      id: 0,
+      visible: false
     }
   ])
+
+  function handleBurgerClick(e) {
+    document.querySelector(".burger-underlay").classList.toggle("toggle");
+    document.querySelector(".burger").classList.toggle("toggle");    
+    toggleSubMenu(0);
+  }
 
   function toggleSubMenu(payload) {
     const nextSettings = settings.map(item => {
@@ -34,11 +44,13 @@ export default function Header(props) {
   }
 
   function subNavItemClick() {
-    console.log('Clicked');
     const nextSettings = settings.map(item => {
       item.visible = false;
       return item;
     });
+
+    document.querySelector(".burger-underlay").classList.remove("toggle");
+    document.querySelector(".burger").classList.remove("toggle");
     setSettings(nextSettings);
   }
 
@@ -53,7 +65,13 @@ export default function Header(props) {
               </Link>
             </div>
             {/* TODO: this will become a burgermenu */}
-            <div className="col-span-1 lg:hidden py-2"></div>
+            <div className="col-span-1 flex justify-end items-center lg:hidden py-2">
+              <div className="burger" onClick={handleBurgerClick}>
+                <div className="line1"></div>
+                <div className="line2"></div>
+                <div className="line3"></div>
+              </div>
+            </div>
             {/* TODO: this will become a burgermenu END */}
             <div className="hidden lg:flex lg:col-span-6 gap-x-4 justify-center items-center py-3">
               <div className="mx-8">
@@ -68,6 +86,7 @@ export default function Header(props) {
             </div>
           </div>
         </nav>
+        <div className="burger-underlay lg:hidden" onClick={handleBurgerClick}></div>
         <SubMenu {...props.pages} settings={settings} subNavItemClick={subNavItemClick}></SubMenu>
       </header>
     </>
@@ -75,7 +94,7 @@ export default function Header(props) {
 }
 
 function SubMenu(props) {
-  // console.log(props);
+  console.log(props);
   const productsId = "cG9zdDo0MzA=";
   const learnId = "cG9zdDo0MDY=";
   const aboutId = "cG9zdDo0MjI=";
@@ -94,9 +113,9 @@ function SubMenu(props) {
   });
 
   return (
-    <nav className={"hidden lg:block bg-gray-header w-full relative z-20 transition-all duration-500 shadow " + (props.settings[0].visible || props.settings[1].visible || props.settings[2].visible ?  "translate-y-0" : "-translate-y-20")}>
-      <div className="2xl:container mx-auto grid grid-cols-6 gap-x-4 px-8 py-2">
-        <div className="flex justify-center items-center col-span-6">
+    <nav className={"sub-menu " + (props.settings[3].visible || props.settings[0].visible || props.settings[1].visible || props.settings[2].visible ?  "translate-x-0 lg:translate-y-0" : "translate-x-full lg:translate-x-0 lg:-translate-y-20")}>
+      <div className="2xl:container mx-auto px-8 py-2">
+        <div className="hidden lg:flex justify-center items-center">
           {
             (props.settings[0].visible ? productPages : props.settings[1].visible ? learnPages :  aboutPages)
             .map(item => {
@@ -106,6 +125,32 @@ function SubMenu(props) {
             })
           }
         </div>
+        <div className="lg:hidden grid grid-cols-6 gap-4 px-8 pt-8">
+          <div className="col-span-6 text-center mb-4">
+            <h4 className="font-bold mb-2">Produkter</h4>
+            {productPages.map(item => {
+              return (
+                <SubNavItem key={item.id} {...item} subNavItemClick={props.subNavItemClick}></SubNavItem>
+              )
+            })}
+          </div>
+          <div className="col-span-6 text-center mb-4">
+            <h4 className="font-bold mb-2">Lær at strikke</h4>
+            {learnPages.map(item => {
+              return (
+                <SubNavItem key={item.id} {...item} subNavItemClick={props.subNavItemClick}></SubNavItem>
+              )
+            })}
+          </div>
+          <div className="col-span-6 text-center mb-4">
+            <h4 className="font-bold mb-2">Om os</h4>
+            {aboutPages.map(item => {
+              return (
+                <SubNavItem key={item.id} {...item} subNavItemClick={props.subNavItemClick}></SubNavItem>
+              )
+            })}
+          </div>
+        </div>
       </div>
     </nav>
   )
@@ -113,8 +158,6 @@ function SubMenu(props) {
 }
 
 function SubNavItem(props) {
-  console.log(props);
-
   return (
     <div className="mx-8">
       <Link href={`/${props.ancestors.edges[0].node.slug}/${props.slug}`}>
