12bbfba4cb2f9fcf808c044f496620b3b0ad0214
Morten Gross <mg@vuffeli.dk>
2021-12-16 23:35:26 +0100

    added head

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,6 +3,13 @@ import { getData } from "../../lib/api";
 export default function Custom404(props) {
     return (
       <>
+        <Head>
+          <title>{`${props.headerFooterData.generalSettings.title} | 404 - Page not found`}</title>
+          <meta name="robots" content="noindex"></meta>
+          <link rel="icon" type="image/png" sizez="180x180" href="./apple-touch-icon.ico"></link>
+          <link rel="icon" type="image/png" sizez="32x32" href="./favicon32x32"></link>
+          <link rel="icon" type="image/png" sizez="16x16" href="./favicon16x16"></link>
+        </Head>
         <h1>404 - Page Not Found</h1>
       </>
     )
--- src/pages/500.js
+++ src/pages/500.js
@@ -1,8 +1,15 @@
 import { getData } from "../../lib/api";
+import Head from "next/image";
 
 export default function Custom500(props) {
   return (
-    <>
+      <><Head>
+      <title>{`${props.headerFooterData.generalSettings.title} | 500 - Server-side error occured`}</title>
+        <meta name="robots" content="noindex"></meta>
+      <link rel="icon" type="image/png" sizez="180x180" href="./apple-touch-icon.ico"></link>
+      <link rel="icon" type="image/png" sizez="32x32" href="./favicon32x32"></link>
+      <link rel="icon" type="image/png" sizez="16x16" href="./favicon16x16"></link>
+      </Head>
       <h1>500 - Server-side error occurred</h1>
     </>
   )
