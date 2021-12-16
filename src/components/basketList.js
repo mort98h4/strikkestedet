a8badd0ad617b7b1611226700bd51f389910d3b8
Morten Gross <mg@vuffeli.dk>
2021-12-16 16:53:29 +0100

    added image tag

--- src/components/basketList.js
+++ src/components/basketList.js
@@ -1,4 +1,5 @@
 import clsx from "clsx";
+import Image from 'next/image';
 
 export default function BasketList(data) {
   const info = data.data[0];
@@ -9,14 +10,20 @@ export default function BasketList(data) {
     <>
       <hr className="h-0.2 bg-black" />
       <li className="row-span-1 grid-cols-2 gap-y-10 lg:gap-y-0 md:bg-white p-4 grid lg:grid-cols-4 lg:gap-4">
-        <img src={color.guid} alt="" width="100px" height="100px" />
-        <div>
+        <div className="lg:col-span-2 flex">
+          <div className="block relative w-full max-w-[100px]">
+          <Image src={color.guid} alt="" width="50" height="50" layout="responsive"/>
+        </div>
+        
+        <div className="ml-4 self-center">
           <h3 className="font-bold">{info.title}</h3>
           <span className="text-black-70">
             Farve: <span>{color.title}</span>
           </span>
         </div>
-        <div>
+        </div>
+        
+        <div className="self-center">
           <h3 className="font-bold">Antal</h3>
           <div className="flex flex-row h-10 gap-1">
             <button
@@ -42,7 +49,7 @@ export default function BasketList(data) {
             </button>
           </div>
         </div>
-        <div>
+        <div className="self-center">
           <h3 className="font-bold">Pris</h3>
           <span className="text-black-70 block">
             Stk: <span className="ml-6">{info.price} DKK</span>
